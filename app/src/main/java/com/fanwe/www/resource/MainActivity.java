3762fa013996eb8c012030b23e0c66ff09095d04
zjun <565061763@qq.com>
2017-07-29 15:32:22 +0800

    新增xml

--- app/src/main/java/com/fanwe/www/resource/MainActivity.java
+++ app/src/main/java/com/fanwe/www/resource/MainActivity.java
@@ -3,8 +3,6 @@ package com.fanwe.www.resource;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -14,16 +12,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.res_selector_white_gray).setOnClickListener(this);
-        findViewById(R.id.res_selector_white_gray_stroke1px_all).setOnClickListener(this);
-        findViewById(R.id.res_selector_white_gray_stroke1px_b).setOnClickListener(this);
-        findViewById(R.id.res_selector_white_gray_stroke1px_blr).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v)
     {
-        Toast.makeText(this, ((TextView) v).getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
